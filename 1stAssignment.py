f7014183653d02349340f6749549e7bb8fcf9f10
Kodesignpy <67595142+Kodesignpy@users.noreply.github.com>
2020-06-29 10:15:32 -0500

    Add files via upload

---
+++ 1stAssignment.py
@@ -0,0 +1,46 @@
+"""#1"""
+"""number = str(input("Is 19 even or odd?"))
+
+if number== "even":
+    print("Completely Wrong")
+elif number=="odd":
+    print("Corrrrrrrrrrrect!!!!")
+else:
+    print("Try Again")"""
+
+
+"""a=int(input("enter your first value:" ))
+b=int(input("enter your second value:"))
+c=int(input("enter your final value:"))
+if a>b>c:
+    print( "The first is your biggest number")
+    print( "The second is your middle number")
+    print("The last is your smallest number")
+elif b>a>c:
+    print("The second is your biggest number")
+    print("The first is your middle number")
+    print("The last is your smallest number")
+elif a>c>b:
+    print( "The first is your biggest number")
+    print( "The last is your middle number")
+    print("The second is your smallest number")
+elif b>c>a:
+    print("The second is your biggest number")
+    print("The last is your middle number")
+    print("The first is your smallest number")
+elif c>a>b:
+    print("The last is your biggest number")
+    print("The first is your middle number")
+    print("The second is your smallest number")
+elif c>b>a:
+    print("The last is your biggest number")
+    print("The second is your middle number")
+    print("The first is your smallest number")
+else:
+   print("try again")"""
+
+"""div=int(input("This is the Divider by Five Checker \n Input your number:"))
+if div/5:
+    print("It is divisible!")
+else:
+    print("It is not divisible:(")"""
